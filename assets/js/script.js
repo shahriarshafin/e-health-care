fa16c6de0e7c990d05749878ba332092cfc72482
shafin <shafinshahriar340@gmail.com>
2021-09-01 18:47:15 +0600

    favicon added, disease search section updated, API implemented

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,89 +1,33 @@
+// function call
+getCovidapi();
+getdiseaseName();
+
 // covid-19-api
 async function getCovidapi() {
 	const jsondata = await fetch("https://api.covid19api.com/summary");
 	const jsdata = await jsondata.json();
 	const Bangladesh = jsdata.Countries[13];
 
-	console.log(Bangladesh.NewConfirmed);
 	document.querySelector("#confirmed").innerHTML = Bangladesh.NewConfirmed;
 	document.querySelector("#deaths").innerHTML = Bangladesh.NewDeaths;
 	document.querySelector("#recovered").innerHTML = Bangladesh.NewRecovered;
 }
-getCovidapi();
-// covid-19-api
 
-// taglist
-// const huu = fetch("./test.json")
-// 	.then((response) => response.json())
-// 	.then((json) => {
-// 		console.table(json);
-// 		for (var x in json) {
-// 			document.write(json[x]);
-// 			let sexy = [json[x]];
-// 			return sexy;
-// 		}
-// 	});
+// getdiseaseName
+async function getdiseaseName() {
+	const jsondatas = await fetch(
+		"https://gist.githubusercontent.com/shahriarshafin/2d164de88020efb2268db7b06281d87c/raw/89d4186b920ea160481f22fb3d988943fa09ac4c/disease.json"
+	);
+	const diseases = await jsondatas.json();
 
-// ..........
-
-// const myJSON = '["Ford", "BMW", "Fiat"]';
-// const diseases = JSON.parse(myJSON);
-
-// .................................................
-const diseases = [
-	"Allergies",
-	"Colds and Flu",
-	"Conjunctivitis",
-	"Diarrhea",
-	"Headaches",
-	"Mononucleosis",
-	"Stomach Aches",
-	"irritation",
-	"Runny nose",
-	"Stuffy nose",
-	"Puffy",
-	"Sneezing",
-	"Inflamed",
-	"sneezing,",
-	"sore throat",
-	"cough",
-];
-var input = document.querySelector("input[name=tags-manual-suggestions]"),
-	// init Tagify script on the above inputs
-	tagify = new Tagify(input, {
-		whitelist: diseases,
-		dropdown: {
-			position: "manual",
-			maxItems: Infinity,
-			enabled: 0,
-			classname: "customSuggestionsList",
-		},
-		enforceWhitelist: true,
-	});
-
-tagify
-	.on("dropdown:show", onSuggestionsListUpdate)
-	.on("dropdown:hide", onSuggestionsListHide)
-	.on("dropdown:scroll", onDropdownScroll);
-
-renderSuggestionsList();
-
-// ES2015 argument destructuring
-function onSuggestionsListUpdate({ detail: suggestionsElm }) {
-	console.log(suggestionsElm);
+	let input = document.querySelector("textarea[name=tags2]"),
+		tagify = new Tagify(input, {
+			enforceWhitelist: true,
+			delimiters: null,
+			whitelist: diseases,
+			callbacks: {
+				add: console.log, // callback when adding a tag
+				remove: console.log, // callback when removing a tag
+			},
+		});
 }
-
-function onSuggestionsListHide() {
-	console.log("hide dropdown");
-}
-
-function onDropdownScroll(e) {
-	console.log(e.detail);
-}
-
-// https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentElement
-function renderSuggestionsList() {
-	tagify.dropdown.show(); // load the list
-	tagify.DOM.scope.parentNode.appendChild(tagify.DOM.dropdown);
-}
-// taglist
