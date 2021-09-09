3fb166923deb3309cf6058cfb8f5ec9d334db2ce
shafin <shafinshahriar340@gmail.com>
2021-09-09 16:15:37 +0600

    covid page added

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ async function getCovidapi() {
 	const jsondata = await fetch("https://api.covid19api.com/summary");
 	const jsdata = await jsondata.json();
 	const Bangladesh = jsdata.Countries[13];
-
+	console.log(jsdata);
 	document.querySelector("#confirmed").innerHTML = Bangladesh.NewConfirmed;
 	document.querySelector("#deaths").innerHTML = Bangladesh.NewDeaths;
 	document.querySelector("#recovered").innerHTML = Bangladesh.NewRecovered;
