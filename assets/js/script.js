a46f4a0d27a2ea89698d7920399ea991898d3e95
shafin <shafinshahriar340@gmail.com>
2021-09-22 05:51:20 +0600

    field autocomplete, json model and data updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,17 @@
-// function call
-getCovidapi();
 getdiseaseName();
 
-// covid-19-api
-async function getCovidapi() {
-	const jsondata = await fetch("https://api.covid19api.com/summary");
-	const jsdata = await jsondata.json();
-	const Bangladesh = jsdata.Countries[13];
-	console.log(jsdata);
-	document.querySelector("#confirmed").innerHTML = Bangladesh.NewConfirmed;
-	document.querySelector("#deaths").innerHTML = Bangladesh.NewDeaths;
-	document.querySelector("#recovered").innerHTML = Bangladesh.NewRecovered;
-}
-
-// getdiseaseName
 async function getdiseaseName() {
-	const jsondatas = await fetch(
-		"https://gist.githubusercontent.com/shahriarshafin/2d164de88020efb2268db7b06281d87c/raw/89d4186b920ea160481f22fb3d988943fa09ac4c/disease.json"
-	);
-	const diseases = await jsondatas.json();
+	const jsondatas = await fetch("./assets/diseases.json");
+	const getData = await jsondatas.json();
+
+	const diseases = getData.diseases;
+	const cities = getData.cities;
+	const doctorSpe = getData.doctorSpe;
+
+	autocomplete(document.getElementById("cityField"), cities);
+	autocomplete(document.getElementById("cityField2"), cities);
+	autocomplete(document.getElementById("doctorSpeField"), doctorSpe);
+	autocomplete(document.getElementById("doctorSpeField2"), doctorSpe);
 
 	let input = document.querySelector("textarea[name=tags2]"),
 		tagify = new Tagify(input, {
@@ -30,4 +23,78 @@ async function getdiseaseName() {
 				remove: console.log, // callback when removing a tag
 			},
 		});
+}
+
+function autocomplete(inp, arr) {
+	var currentFocus;
+	inp.addEventListener("input", function (e) {
+		var a,
+			b,
+			i,
+			val = this.value;
+		closeAllLists();
+		if (!val) {
+			return false;
+		}
+		currentFocus = -1;
+		a = document.createElement("DIV");
+		a.setAttribute("id", this.id + "autocomplete-list");
+		a.setAttribute("class", "autocomplete-items");
+		this.parentNode.appendChild(a);
+		for (i = 0; i < arr.length; i++) {
+			if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+				b = document.createElement("DIV");
+				b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+				b.innerHTML += arr[i].substr(val.length);
+				b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+				b.addEventListener("click", function (e) {
+					inp.value = this.getElementsByTagName("input")[0].value;
+					closeAllLists();
+				});
+				a.appendChild(b);
+			}
+		}
+	});
+	inp.addEventListener("keydown", function (e) {
+		var x = document.getElementById(this.id + "autocomplete-list");
+		if (x) x = x.getElementsByTagName("div");
+		if (e.keyCode == 40) {
+			currentFocus++;
+			addActive(x);
+		} else if (e.keyCode == 38) {
+			currentFocus--;
+			addActive(x);
+		} else if (e.keyCode == 13) {
+			e.preventDefault();
+			if (currentFocus > -1) {
+				if (x) x[currentFocus].click();
+			}
+		}
+	});
+
+	function addActive(x) {
+		if (!x) return false;
+		removeActive(x);
+		if (currentFocus >= x.length) currentFocus = 0;
+		if (currentFocus < 0) currentFocus = x.length - 1;
+		x[currentFocus].classList.add("autocomplete-active");
+	}
+
+	function removeActive(x) {
+		for (var i = 0; i < x.length; i++) {
+			x[i].classList.remove("autocomplete-active");
+		}
+	}
+
+	function closeAllLists(elmnt) {
+		var x = document.getElementsByClassName("autocomplete-items");
+		for (var i = 0; i < x.length; i++) {
+			if (elmnt != x[i] && elmnt != inp) {
+				x[i].parentNode.removeChild(x[i]);
+			}
+		}
+	}
+	document.addEventListener("click", function (e) {
+		closeAllLists(e.target);
+	});
 }
